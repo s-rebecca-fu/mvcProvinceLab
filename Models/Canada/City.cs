f8146fb60476c434ca9bbfa42a442ce4a5a5b816
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-02-02 02:59:48 -0500

    province display changed

--- Models/Canada/City.cs
+++ Models/Canada/City.cs
@@ -14,6 +14,7 @@ namespace CanadaLab.Models.Canada
         [Display(Name = "City"), Required]
         public string CityName { get; set; }
         public int Popuation { get; set; }
+        [Display(Name = "Province"), Required]
         public string ProvinceCode { get; set; }
         public Province Province { get; set; }
     }
